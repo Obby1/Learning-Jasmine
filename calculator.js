7c7bf952350f80c1640935b3bbd0779b61bbf7b8
Obby <obbydelrosario@gmail.com>
2022-09-26 15:20:55 -0700

    updated functions

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+
+
+
 window.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById("calc-form");
   if (form) {
@@ -8,6 +11,8 @@ window.addEventListener('DOMContentLoaded', function() {
     });
   }
 });
+
+
 
 function getCurrentUIValues() {
   return {
@@ -21,20 +26,63 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const loanAmount = document.querySelector(`#loan-amount`);
+  const loanYears = document.querySelector(`#loan-years`);
+  const loanRate = document.querySelector(`#loan-rate`);
+  loanAmount.value = 10000;
+  loanYears.value = 10;
+  loanRate.value = 10;
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+const currentValues = getCurrentUIValues();
+updateMonthly(calculateMonthlyPayment(currentValues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const monthlyRate = (values.rate / 100) / 12;
+  const n = Math.floor(values.years * 12);
+  return (
+    (monthlyRate * values.amount) /
+    (1 - Math.pow((1 + monthlyRate), -n))
+  ).toFixed(2);
+
+
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyUI = document.getElementById("monthly-payment");
+  monthlyUI.innerText = "$" + monthly;
 }
+
+
+// old code:
+// let loanMonths = loanYears*12;
+//   const interest = (loanAmount *(loanRate))/ (loanMonths);
+//   console.log(interest);
+//   const monthlyPaymentValue = ((loanAmount/(loanMonths))+interest).toFixed(2);
+//   return monthlyPaymentValue;
+
+
+// function calculateMonthlyPayment(loanAmount, loanYears, loanRate) {
+//   loanAmount = Math.round(document.querySelector(`#loan-amount`).value*100)/100;
+//   loanYears = document.querySelector(`#loan-years`).value;
+//   loanRate = parseFloat(document.querySelector(`#loan-rate`).value);
+//   numerator = Math.round((loanAmount*loanRate)*100)/100;
+//   console.log(numerator);
+//   denominator = 1-(1+loanRate)**(-(loanYears*12));
+//   console.log(denominator);
+//   let monthlyPaymentValue = numerator/denominator
+//   console.log(monthlyPaymentValue);
+//   return monthlyPaymentValue
+
+
+// }
